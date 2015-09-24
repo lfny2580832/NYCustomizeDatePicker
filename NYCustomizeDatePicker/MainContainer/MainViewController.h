1c6cc8f058b419b168725a0213a67f54dc161288
niuyan <632176758@qq.com>
2015-09-24 23:50:20 +0800

    project init

---
+++ NYCustomizeDatePicker/MainContainer/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  NYCustomizeDatePicker
+//
+//  Created by 牛严 on 15/9/24.
+//  Copyright (c) 2015年 牛严. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ NYCustomizeDatePicker/MainContainer/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  MainViewController.m
+//  NYCustomizeDatePicker
+//
+//  Created by 牛严 on 15/9/24.
+//  Copyright (c) 2015年 牛严. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *contentLabel;
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (IBAction)showCustomeDatePicker:(id)sender {
+    
+}
+
+@end

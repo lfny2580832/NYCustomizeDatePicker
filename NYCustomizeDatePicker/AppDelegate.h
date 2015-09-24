84b28465696c151d989c40a5cd6451214e150315
niuyan <632176758@qq.com>
2015-09-24 23:34:05 +0800

    Initial Commit

---
+++ NYCustomizeDatePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NYCustomizeDatePicker
+//
+//  Created by 牛严 on 15/9/24.
+//  Copyright (c) 2015年 牛严. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
